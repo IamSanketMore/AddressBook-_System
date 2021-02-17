af6996a39935b95bd30e91694fd4cfcf8594dd82
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-17 16:02:27 +0530

    UC-1:-Ability to Create A contact in Address Book System

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,7 +1,47 @@
 public class AddressBookSystem
 {
-	public static void main(String args[])
+	public static void main(String args [])
 	{
-		System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch");
+		System.out.println("************************************************************************************");
+		System.out.println("*                                Address Book System                               *");
+		System.out.println("************************************************************************************");
+		
+		Contact object = new Contact("Sanket","More","Deopur Dhule","Dhule","Maharastra","424005","8180019348","sanketmore.999.m@gmail.com");
+		
+		object.contacts();
 	}
+}
+class Contact
+{
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String state;
+		String zip;
+		String mobileNumber;
+		String email;
+	
+		Contact(String firstName, String lastName,String address,String city,String state,String zip,String mobileNumber,String email)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.zip = zip;
+			this.mobileNumber = mobileNumber;
+			this.email  = email;
+		}
+		public void contacts()
+		{
+			System.out.println("First Name:-"+firstName);
+			System.out.println("Last Name:-"+lastName);
+			System.out.println("Address:-"+address);
+			System.out.println("City:-"+city);
+			System.out.println("State:-"+state);
+			System.out.println("Zip Code:-"+zip);
+			System.out.println("Mobile Number:-"+mobileNumber);
+			System.out.println("Email Id:-"+email);
+		}
 }
