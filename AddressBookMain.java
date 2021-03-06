3263a3080f587f084f5f5a2ea377be7155df953d
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-06 19:40:28 +0530

    Multiple address Book Main Class

---
+++ AddressBookMain.java
@@ -0,0 +1,14 @@
+
+public class AddressBookMain
+{		
+	public static void main(String args[])
+	{
+		System.out.println("************************************************************************************");
+		System.out.println("*                                Address Book System                               *");
+		System.out.println("************************************************************************************");
+
+		BookLogic BookLogic = new BookLogic();
+		BookLogic.addressBookDetails();
+	}// main methood end
+
+}
