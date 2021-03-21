0289e14bf362e2e8ade20bf8094baa302b7b19fd
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-21 20:33:54 +0530

    Multiple Address Book Main Method Classs

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,14 +1,13 @@
-
 public class AddressBookMain
-{		
-	public static void main(String args[])
-	{
-		System.out.println("************************************************************************************");
-		System.out.println("*                                Address Book System                               *");
-		System.out.println("************************************************************************************");
+{
+    public static void main(String [] args)
+    {
+        System.out.println("************************************************************************************");
+        System.out.println("*                                Address Book System                               *");
+        System.out.println("************************************************************************************");
 
-		BookLogic BookLogic = new BookLogic();
-		BookLogic.addressBookDetails();
-	}// main methood end
+        BookLogic BookLogic = new BookLogic();
+        BookLogic.addressBookDetails();
+    }// main methood end
 
 }
